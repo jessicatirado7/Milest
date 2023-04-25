77c90085a9ea8c39d0900e248a58e473dca91777
Jessica <jvtdesigns7@gmail.com>
2023-04-25 18:36:55 -0500

    Added Parcel for opt.

---
+++ dist/index.4dc5684f.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - Creative v7.0.5 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+window.addEventListener("DOMContentLoaded",(n=>{var o=function(){const n=document.body.querySelector("#mainNav");n&&(0===window.scrollY?n.classList.remove("navbar-shrink"):n.classList.add("navbar-shrink"))};o(),document.addEventListener("scroll",o);document.body.querySelector("#mainNav")&&new bootstrap.ScrollSpy(document.body,{target:"#mainNav",offset:74}),$(".navbar-collapse a:not(.dropdown-toggle)").click((function(){$(".navbar-collapse").collapse("hide")})),AOS.init()}));
+//# sourceMappingURL=index.4dc5684f.js.map
